745ea45954c9c0f67211c9953e141e3deb0c68c2
carsonlevine <carsonglevine@me.com>
2016-07-08 15:44:40 -0400

    first commit

---
+++ DictionariesPractice.playground/Contents.swift
@@ -0,0 +1,44 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//A runner created two arrays to track the days that he ran and the distances that he ran. The distanceArray is listed in the same order as the dateArray
+
+
+var dateArray: [String] = ["4/3/2015", "4/5/2015", "4/6/2015", "4/7/2015"]
+var distanceArray: [Double] = [6, 4, 5.5, 4.5]
+
+
+
+//1. Create a dictionary of type [String : Double] called runTracker where the keys are the dates of his workouts and the values are the distances of each workout.
+
+
+
+
+
+//2. Write a function called displayWorkouts. The function should take in one parameter called workouts of type [String : Double]. The function should print out a list of workouts in a clean format.
+//displayWorkouts(runTracker)
+//prints:
+//   Date   |  Milage
+//--------------------
+// 4/3/2015 |    6
+// 4/5/2015 |    4
+// 4/6/2015 |   5.5
+// 4/7/2015 |   4.5
+
+
+
+//3. Add the following workout to the runTracker dictionary: date: 4/9/2015, distance: 7 miles. The run displayWorkouts again to make sure it was added correctly.
+
+
+
+//4. Remove the workout from 4/5/2015
+
+
+
+//5. Update the workout from 4/6/2015, giving it a distance of 6 miles
+
+
+
+
+
