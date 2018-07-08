33bbd6df1dd743c61a3fd0cb25e59395a349e81f
Jeff <jolson615@gmail.com>
2018-07-08 18:17:34 -0400

    Update Contents.swift

--- DictionariesPractice.playground/Contents.swift
+++ DictionariesPractice.playground/Contents.swift
@@ -1,19 +1,24 @@
-//A runner created two arrays to track the days that he ran and the distances that he ran. The distanceArray is listed in the same order as the dateArray
+//A runner named Brian created two arrays to track the days that he ran and the distances that he ran. The distanceArray is listed in the same order as the dateArray
 
 
-var dateArray: [String] = ["4/3/2015", "4/5/2015", "4/6/2015", "4/7/2015"]
+var dateArray: [String] = ["7/3/2018", "7/5/2018", "7/6/2018", "7/7/2018"]
 var distanceArray: [Double] = [6, 4, 5.5, 4.5]
 
 
 
-//1. Create a dictionary of type [String : Double] called runTracker where the keys are the dates of his workouts and the values are the distances of each workout.
+//1. Create a dictionary of type [String : Double] called briansRuns where the keys are the dates of his workouts and the values are the distances of each workout.
 
 
 
 
 
-//2. Write a function called displayWorkouts. The function should take in one parameter called workouts of type [String : Double]. The function should print out a list of workouts in a clean format.
-//displayWorkouts(runTracker)
+//2. Create a second dictionary of the same time called myRuns where you key some dates of your own workouts. If you don't run, that's fine; you can make them up. 
+
+
+
+
+//3. Write a function called displayWorkouts. The function should take in one parameter called workouts of type [String : Double]. The function should print out a list of workouts in a clean format.
+//displayWorkouts(workouts: briansRuns)
 //prints:
 //   Date   |  Milage
 //--------------------
@@ -24,15 +29,20 @@ var distanceArray: [Double] = [6, 4, 5.5, 4.5]
 
 
 
-//3. Add the following workout to the runTracker dictionary: date: 4/9/2015, distance: 7 miles. The run displayWorkouts again to make sure it was added correctly.
+//4. Confirm that the displayWorkouts function works for ANYONE's running dictionary by calling it with the myRuns dictionary.
 
 
 
-//4. Remove the workout from 4/5/2015
+
+//5. Add the following workout to the runTracker dictionary: date: 7/9/2018, distance: 7 miles. The run displayWorkouts again to make sure it was added correctly.
 
 
 
-//5. Update the workout from 4/6/2015, giving it a distance of 6 miles
+//6. Remove the workout from 7/5/2018.
+
+
+
+//5. Update the workout from 7/6/2018, giving it a distance of 6 miles.
 
 
 
